dc9551caf4285b8fc942e27f608bfa49c041bad0
prateekpriyadarshi <prateekpriyadarshi1328@gmail.com>
2025-06-09 00:50:26 +0530

    Initial Commit

---
+++ SwiftStarterTests/SwiftStarterTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStarterTests.swift
+//  SwiftStarterTests
+//
+//  Created by Prateek Priyadarshi on 09/06/25.
+//
+
+import Testing
+@testable import SwiftStarter
+
+struct SwiftStarterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
